eaeed5dd9db1f4e592e263593cbcbe5c8262c8a2
Vikas Pandya <vpandya@juniper.net>
2017-10-30 23:48:32 -0700

    added json formatted output

--- count_hours.py
+++ count_hours.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from datetime import datetime
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -11,11 +12,14 @@ today_6pm = time_now.replace(hour=18, minute=0, second=0, microsecond=0)
 @app.route("/")
 def count_hours():
     # open_hours = "8AM-6PM PST"
+    data = {}
+    data['hours'] = '8AM-6PM PST'
     if today_8am <= time_now <= today_6pm:
-        result = "Open"
+        data['result'] = 'Open'
     else:
-        result = "Close"
-    return result
+        data['result'] = 'Close'
+
+    return jsonify(**data)
 
 if __name__ == "__main__":
     app.run()
